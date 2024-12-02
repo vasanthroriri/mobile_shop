39737588794fac4b886d4806942efe307875a6b0
vasanthroriri <vasanth@roririsoft.com>
2024-12-02 22:58:19 +0530

    link update

--- head.php
+++ head.php
@@ -36,10 +36,11 @@
     
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;700&display=swap" rel="stylesheet">
     <!-- Bootstrap CSS -->
-<link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
+<!-- <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet"> -->
+<link href="assets/css/boostrao-4-5-2.css" rel="stylesheet" type="text/css" />
 <!-- Bootstrap Icons CSS -->
-<link href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-icons/1.5.0/font/bootstrap-icons.min.css" rel="stylesheet">
-
+<!-- <link href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-icons/1.5.0/font/bootstrap-icons.min.css" rel="stylesheet"> -->
+<link href="assets/css/ajax.css" rel="stylesheet" type="text/css" />
 
     
 
